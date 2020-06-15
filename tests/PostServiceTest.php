b3b3ffc8f8094f40cd5dcdcce2d4a0c743c76695
Petr Tobias <petr.tobias@plus4u.net>
2020-06-15 17:25:07 +0200

    Updated PostServiceTest

--- tests/PostServiceTest.php
+++ tests/PostServiceTest.php
@@ -1,19 +1,53 @@
 <?php
 namespace IW\Tests\Workshop;
-use Mockery\Adapter\Phpunit\MockeryTestCase;
+use IW\Workshop\Client\Curl;
+use IW\Workshop\PostService;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
 
 
-class PostServiceTest extends MockeryTestCase {
-  public function tearDown() {
-    \Mockery::close();
-  }
-
+class PostServiceTest extends TestCase {
   public function testCreatePost() {
-    $mock = \Mockery::mock('PostService');
-    $mock->shouldReceive('createPost')->with([1, "text"])->andReturn(1);
+    $mock = $this->createMock(Curl::class);
 
-    $mockResult = $mock->createPost([1, "text"]);
+    $input = ['id' => 1];
+
+    $mock->method('post')->willReturn([201, json_encode($input)]);
+
+    $postService = new PostService($mock);
+
+    $mockResult = $postService->createPost($input);
 
     $this->assertEquals(1, $mockResult);
+  }
+
+  public function testBadStatusCode() {
+    $mock = $this->createMock(Curl::class);
+
+    $input = ['id' => 1];
+
+    $mock->method('post')->willReturn([400, json_encode($input)]);
+
+    $postService = new PostService($mock);
+
+    $this->expectException(RuntimeException::class);
+    $this->expectExceptionMessage('Post could not be created.');
+    $postService->createPost($input);
+
+  }
+
+  public function testIdDoesNotExist() {
+    $mock = $this->createMock(Curl::class);
+
+    $input = ['aaa' => 1];
+
+    $mock->method('post')->willReturn([201, json_encode($input)]);
+
+    $postService = new PostService($mock);
+
+    $this->expectException(RuntimeException::class);
+    $this->expectExceptionMessage('An id of article could not be retrieved.');
+    $postService->createPost($input);
+
   }
 }
